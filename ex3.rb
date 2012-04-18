ff4465fbd0c90db463ea62b8f7be4c6aac095385
Dalton Barreto <daltonmatos@gmail.com>
2012-04-18 19:05:39 -0300

    Ex2 extras

--- ex3.rb
+++ ex3.rb
@@ -1,11 +1,11 @@
 puts "I will now count my chickens:"
 
-puts "Hens", 25 + 30 / 6
+puts "Hens", (25.0 + 30) / 6
 puts "Roosters", 100 - 25 * 3 % 4
 
 puts "Now I will count the eggs:"
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6.0
 
 puts "Is it true that 3 + 2 < 5 - 7?"
 
